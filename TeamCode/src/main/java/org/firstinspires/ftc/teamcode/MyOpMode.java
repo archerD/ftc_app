e4ab90ed6abce22eafe5bfab3611dc5faf0ce495
David <dflores0818@gmail.com>
2017-07-31 19:24:11 -0700

    Created file MyOpMode.java.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyOpMode.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by dflor on 7/31/2017.
+ */
+
+public class MyOpMode extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
