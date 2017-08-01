f22b9ff092fe4e4523c68185ee63b6dbeefb5e2b
David <dflores0818@gmail.com>
2017-07-31 19:35:28 -0700

    Some sketcy changes.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyOpMode.java
@@ -1,19 +1,150 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
+import com.qualcomm.robotcore.hardware.configuration.MotorConfigurationType;
 
 /**
  * Created by dflor on 7/31/2017.
  */
 
 public class MyOpMode extends OpMode {
+    DcMotor myMotor;
+
     @Override
     public void init() {
+        myMotor = new DcMotor() {
+            @Override
+            public MotorConfigurationType getMotorType() {
+                return null;
+            }
+
+            @Override
+            public void setMotorType(MotorConfigurationType motorType) {
+
+            }
+
+            @Override
+            public DcMotorController getController() {
+                return null;
+            }
+
+            @Override
+            public int getPortNumber() {
+                return 0;
+            }
+
+            @Override
+            public void setZeroPowerBehavior(ZeroPowerBehavior zeroPowerBehavior) {
+
+            }
+
+            @Override
+            public ZeroPowerBehavior getZeroPowerBehavior() {
+                return null;
+            }
+
+            @Override
+            public void setPowerFloat() {
+
+            }
+
+            @Override
+            public boolean getPowerFloat() {
+                return false;
+            }
+
+            @Override
+            public void setTargetPosition(int position) {
+
+            }
+
+            @Override
+            public int getTargetPosition() {
+                return 0;
+            }
+
+            @Override
+            public boolean isBusy() {
+                return false;
+            }
+
+            @Override
+            public int getCurrentPosition() {
+                return 0;
+            }
+
+            @Override
+            public void setMode(RunMode mode) {
+
+            }
+
+            @Override
+            public RunMode getMode() {
+                return null;
+            }
+
+            @Override
+            public void setDirection(Direction direction) {
+
+            }
+
+            @Override
+            public Direction getDirection() {
+                return null;
+            }
+
+            @Override
+            public void setPower(double power) {
+
+            }
+
+            @Override
+            public double getPower() {
+                return 0;
+            }
+
+            @Override
+            public Manufacturer getManufacturer() {
+                return null;
+            }
+
+            @Override
+            public String getDeviceName() {
+                return null;
+            }
+
+            @Override
+            public String getConnectionInfo() {
+                return null;
+            }
+
+            @Override
+            public int getVersion() {
+                return 0;
+            }
+
+            @Override
+            public void resetDeviceConfigurationForOpMode() {
+
+            }
+
+            @Override
+            public void close() {
+
+            }
+        };
+
 
     }
 
     @Override
     public void loop() {
+
+        myMotor.setPower(1.2);
 
     }
 }
